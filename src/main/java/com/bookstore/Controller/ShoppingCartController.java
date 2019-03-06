a77eacd21e4a794ac9bb736f97881479fbbcf9fc
Dohvakin <yashkaveeshwar@gmail.com>
2019-03-06 21:25:48 +0530

    Adding Shopping Cart functions to the web site also and fixing some href links
    Signed-off-by: Dohvakin <yashkaveeshwar@gmail.com>

--- src/main/java/com/bookstore/Controller/ShoppingCartController.java
+++ src/main/java/com/bookstore/Controller/ShoppingCartController.java
@@ -1,15 +1,18 @@
 package com.bookstore.controller;
 
 
+import com.bookstore.domain.Book;
 import com.bookstore.domain.CartItem;
 import com.bookstore.domain.ShoppingCart;
 import com.bookstore.domain.User;
+import com.bookstore.service.BookService;
 import com.bookstore.service.CartItemService;
 import com.bookstore.service.ShoppingCartService;
 import com.bookstore.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -28,6 +31,9 @@ public class ShoppingCartController {
 	@Autowired
 	private ShoppingCartService shoppingCartService;
 
+	@Autowired
+	private BookService bookService;
+
 	@RequestMapping("/cart")
 	public String cart(Model model, Principal principal) {
 		User user = userService.findByUsername(principal.getName());
@@ -41,6 +47,23 @@ public class ShoppingCartController {
 		model.addAttribute("shoppingCart", shoppingCart);
 
 		return "shoppingCart";
+	}
+
+	@RequestMapping("/addItem")
+	public String addItem(@ModelAttribute("book") Book book, @ModelAttribute("qty") String qty, Model model, Principal principal) {
+		User user = userService.findByUsername(principal.getName());
+		book = bookService.findOne(book.getId());
+
+		if (Integer.parseInt(qty) > book.getInStockNumber()) {
+			model.addAttribute("notEnoughStock", true);
+			return "forward:/bookDetail?id=" + book.getId();
+		}
+
+		CartItem cartItem = cartItemService.addBookToCartItem(book, user, Integer.parseInt(qty));
+		model.addAttribute("addBookSuccess", toString());
+
+		return "forward:/bookDetail?id=" + book.getId();
+
 	}
 
 }
--- src/main/java/com/bookstore/Service/impl/CartItemServiceImpl.java
+++ src/main/java/com/bookstore/Service/impl/CartItemServiceImpl.java
@@ -1,7 +1,7 @@
 package com.bookstore.service.impl;
 
-import com.bookstore.domain.CartItem;
-import com.bookstore.domain.ShoppingCart;
+import com.bookstore.domain.*;
+import com.bookstore.repository.BookToCartItemRepository;
 import com.bookstore.repository.CartItemRepository;
 import com.bookstore.service.CartItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +16,9 @@ public class CartItemServiceImpl implements CartItemService {
 	@Autowired
 	private CartItemRepository cartItemRepository;
 
+	@Autowired
+	private BookToCartItemRepository bookToCartItemRepository;
+
 	@Override
 	public List<CartItem> findByShoppingCart(ShoppingCart shoppingCart) {
 		return cartItemRepository.findByShoppingCart(shoppingCart);
@@ -28,6 +31,33 @@ public class CartItemServiceImpl implements CartItemService {
 		cartItem.setSubTotal(bigDecimal);
 
 		cartItemRepository.save(cartItem);
+		return cartItem;
+	}
+
+	@Override
+	public CartItem addBookToCartItem(Book book, User user, int qty) {
+		List<CartItem> cartItemList = findByShoppingCart(user.getShoppingCart());
+
+		for (CartItem cartItem : cartItemList) {
+			if (book.getId() == cartItem.getBook().getId()) {
+				cartItem.setQty(cartItem.getQty() + qty);
+				cartItem.setSubTotal(new BigDecimal(book.getOurPrice()).multiply(new BigDecimal(qty)));
+				cartItemRepository.save(cartItem);
+				return cartItem;
+			}
+
+		}
+		CartItem cartItem = new CartItem();
+		cartItem.setShoppingCart(user.getShoppingCart());
+		cartItem.setBook(book);
+		cartItem.setQty(qty);
+		cartItem.setSubTotal(new BigDecimal(book.getOurPrice()).multiply(new BigDecimal(qty)));
+		cartItem = cartItemRepository.save(cartItem);
+		BookToCartItem bookToCartItem = new BookToCartItem();
+		bookToCartItem.setBook(book);
+		bookToCartItem.setCartItem(cartItem);
+		bookToCartItemRepository.save(bookToCartItem);
+
 		return cartItem;
 	}
 }
